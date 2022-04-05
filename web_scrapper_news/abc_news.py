4f81b6de7167477f2ad97aa13d070a1e4d36526a
bs2626 <98107128+bs2626@users.noreply.github.com>
2022-04-05 14:05:15 -0400

    ABC news

---
+++ web_scrapper_news/abc_news.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup as bs
+import csv
+#Load the webpage content
+
+r = requests.get("https://abcnews.go.com/alerts/coronavirus")
+
+#Parse the page into html for accessing the data
+html = r.content
+soup = bs(html,'html.parser')
+
+#Finds out the block with headlines in the form of a list
+h = soup.find_all('div', class_='ContentRoll__Headline')
+
+
+
+
+for i in h:
+    mlink = bs(str(i),'html.parser')
+    title = mlink.find('a')
+    desc = mlink.find('div',class_='ContentRoll__Desc')
+    print(title.string)
+    print(desc.string)
+    print(title.get('href'))
+
+    print("-----------------------")
+
+
+
+
+
