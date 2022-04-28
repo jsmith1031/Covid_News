44ad6b28e8182a3ceb25a89817a1d4ab013cffe0
Jeffrey Smith <jeffreysmith@vt.edu>
2022-04-28 10:08:45 -0400

    Symptom checker page

--- flutter_covid_news/lib/SymptomCheckerPage.dart
+++ flutter_covid_news/lib/SymptomCheckerPage.dart
@@ -4,6 +4,7 @@
 library covid_news;
 
 import 'package:flutter/material.dart';
+import 'main.dart';
 
 class Page2 extends StatelessWidget {
   const Page2({Key? key}) : super(key: key);
@@ -11,41 +12,7 @@ class Page2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('Page 2'),
-      ),
-    );
-  }
-}
-/*
-//part 'newsArticles2.dart';
-class newsArticles {
-  String articleTitle = "";
-  String articleText = "";
-}
-
-Container getArticle(BuildContext context) {
-  print(MediaQuery.of(context).size.height);
-  print(MediaQuery.of(context).size.height * 0.40);
-
-  //build the scrollable grid view
-
-  return Container(
-    child:
-        Text("Hello! i am inside a container!", style: TextStyle(fontSize: 20)),
-    width: 0.3,
-    height: MediaQuery.of(context).size.height * 0.40,
-  );
-}*/
-/*
-class Page2 extends StatelessWidget {
-  const Page2({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+      /*appBar: AppBar(),
       body: const Center(
         child: Text('Page 2'),
       ),
@@ -54,24 +21,24 @@ class Page2 extends StatelessWidget {
 }
 
 class SymptomCheckerWidget extends StatefulWidget {
-  const SymptomCheckerWidget({Key key}) : super(key: key);
+  const SymptomCheckerWidget({Key? key}) : super(key: key);
 
   @override
   _SymptomCheckerWidgetState createState() => _SymptomCheckerWidgetState();
 }
 
 class _SymptomCheckerWidgetState extends State<SymptomCheckerWidget> {
-  String dropDownValue;
+  String dropDownValue = "";
   final scaffoldKey = GlobalKey<ScaffoldState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: scaffoldKey,
+      key: scaffoldKey,*/
       appBar: AppBar(
-        backgroundColor: FlutterFlowTheme.of(context).primaryColor,
+        //backgroundColor: FlutterFlowTheme.of(context).primaryColor,
         automaticallyImplyLeading: false,
-        leading: FFButtonWidget(
+        /*leading: FFButtonWidget(
           onPressed: () async {
             await Navigator.push(
               context,
@@ -98,21 +65,33 @@ class _SymptomCheckerWidgetState extends State<SymptomCheckerWidget> {
             ),
             borderRadius: 12,
           ),
+        ),*/
+        leading: TextButton(
+          style: TextButton.styleFrom(
+            padding: const EdgeInsets.all(16.0),
+            primary: Colors.white,
+            textStyle: const TextStyle(fontSize: 20),
+          ),
+          onPressed: () {
+            //print("Back button");
+            Navigator.of(context).push(_routeBackToMain());
+          },
+          child: const Text('Back'),
         ),
         title: Text(
           'Covid Symptom Checker',
           textAlign: TextAlign.center,
-          style: FlutterFlowTheme.of(context).title2.override(
+          /*style: FlutterFlowTheme.of(context).title2.override(
                 fontFamily: 'Poppins',
                 color: Colors.white,
                 fontSize: 22,
-              ),
+              ),*/
         ),
         actions: [],
         centerTitle: false,
         elevation: 2,
       ),
-      backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+      //backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
       body: SafeArea(
         child: GestureDetector(
           onTap: () => FocusScope.of(context).unfocus(),
@@ -127,12 +106,12 @@ class _SymptomCheckerWidgetState extends State<SymptomCheckerWidget> {
                     Row(
                       mainAxisSize: MainAxisSize.max,
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
+                      /*children: [
                         Text(
                           'Headache?',
-                          style: FlutterFlowTheme.of(context).bodyText1,
+                          //style: FlutterFlowTheme.of(context).bodyText1,
                         ),
-                        FlutterFlowDropDown(
+                        /*FlutterFlowDropDown(
                           options: ['None', 'Mild', 'Severe'].toList(),
                           onChanged: (val) =>
                               setState(() => dropDownValue = val),
@@ -151,8 +130,8 @@ class _SymptomCheckerWidgetState extends State<SymptomCheckerWidget> {
                           borderRadius: 0,
                           margin: EdgeInsetsDirectional.fromSTEB(12, 4, 12, 4),
                           hidesUnderline: true,
-                        ),
-                      ],
+                        ),*/
+                      ],*/
                     ),
                   ],
                 ),
@@ -163,4 +142,13 @@ class _SymptomCheckerWidgetState extends State<SymptomCheckerWidget> {
       ),
     );
   }
-}*/
+}
+
+Route _routeBackToMain() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => const MyApp(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return child;
+    },
+  );
+}
--- flutter_covid_news/lib/main.dart
+++ flutter_covid_news/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Covid News',
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.blue,
       ),
       home: HomePageWidget(),
     );
@@ -57,39 +57,13 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                     borderRadius: BorderRadius.circular(4),
                     child: Stack(
                       children: <Widget>[
-                        Positioned.fill(
-                          child: Container(
-                            decoration: const BoxDecoration(
-                              gradient: LinearGradient(
-                                colors: <Color>[
-                                  Color(0xFF0D47A1),
-                                  Color(0xFF1976D2),
-                                  Color(0xFF42A5F5),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
+                        ButtonColoring(),
                         TextButton(
                           style: TextButton.styleFrom(
                             padding: const EdgeInsets.all(16.0),
                             primary: Colors.white,
                             textStyle: const TextStyle(fontSize: 20),
                           ),
-                          //onPressed: () {
-                          //print('Symptom Checker');
-                          //},
-                          /*onPressed: () async {
-                            await Navigator.push(
-                              //context,
-                              /*PageTransition(
-                              type: PageTransitionType.fade,
-                              duration: Duration(milliseconds: 0),
-                              reverseDuration: Duration(milliseconds: 0),
-                              child: SymptomCheckerWidget(),
-                            ),*/
-                            )
-                          },*/
                           onPressed: () {
                             Navigator.of(context).push(_createRoute());
                           },
@@ -129,6 +103,29 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                   newsGridView(), //This is the scrollable grid view where all the news articles live
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class ButtonColoring extends StatelessWidget {
+  const ButtonColoring({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned.fill(
+      child: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: <Color>[
+              Color(0xFF0D47A1),
+              Color(0xFF1976D2),
+              Color(0xFF42A5F5),
+            ],
+          ),
         ),
       ),
     );
@@ -225,7 +222,8 @@ class topBarButton extends StatelessWidget {
       borderRadius: BorderRadius.circular(4),
       child: Stack(
         children: <Widget>[
-          Positioned.fill(
+          ButtonColoring(),
+          /*Positioned.fill(
             child: Container(
               decoration: const BoxDecoration(
                 gradient: LinearGradient(
@@ -237,7 +235,7 @@ class topBarButton extends StatelessWidget {
                 ),
               ),
             ),
-          ),
+          ),*/
           TextButton(
             style: TextButton.styleFrom(
               padding: const EdgeInsets.all(16.0),
@@ -263,174 +261,12 @@ Route _createRoute() {
     },
   );
 }
-/*
-class Page2 extends StatelessWidget {
-  const Page2({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('Page 2'),
-      ),
-    );
-  }
-}
-
-*/
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-/*
-import 'package:flutter/material.dart';
-import 'newsArticles.dart';
-import 'appTopBar.dart';
-//import 'lib/library.dart';
-
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  void switchScreen() {
-    //setState(() {
-    // This call to setState tells the Flutter framework that something has
-    // changed in this State, which causes it to rerun the build method below
-    // so that the display can reflect the updated values. If we changed
-    // _counter without calling setState(), then the build method would not be
-    // called again, and so nothing would appear to happen.
-  }
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    //print(MediaQuery.of(context).size.height);
-    //print(MediaQuery.of(context).size.width);
-    return MaterialApp(
-      title: 'Covid News',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      //home: const MyHomePage(title: 'Covid News Home Page'),
-      home: MyStatelessWidget(),
-      /*     switchToScreen: FloatingActionButton(
-        onPressed: switchScreen(),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),*/
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: //getArticle(context)),
-            Column(
-          children: <Widget>[
-            Container(
-              child: getNewsSourceLogo(),
-            ),
-            /*Container(
-              child: Text(
-                'Welcome to Prime Message',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    fontFamily: 'Aleo',
-                    fontStyle: FontStyle.normal,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25.0,
-                    color: Colors.black),
-              ),
-            ),*/
-          ],
-        ),
-      ),
-      //body: Center(child: newsArticlesTable(context)),
-    );
-
-    //return TableCreation();
-  }
-
-  Column newsArticlesTable(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            margin: EdgeInsets.all(10),
-            child: Table(
-              border: TableBorder.all(width: 0),
-              children: [
-                TableRow(children: [
-                  Text('Article 1',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  Text('Article 2',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  builtArticle(context),
-                ]),
-                TableRow(children: [
-                  Text('Article 4', textAlign: TextAlign.center),
-                  getNewsSourceLogo(),
-                  //Text('Article 6', textAlign: TextAlign.center),
-                  builtArticle(context),
-                ]),
-              ],
-            ),
-          ),
-        ]);
-  }
-}
 
 Image getNewsSourceLogo() {
   Image abcLogo = Image.network(
@@ -444,15 +280,3 @@ Image getNewsSourceLogo() {
       fit: BoxFit.scaleDown);
   return abcLogo;
 }
-
-Container builtArticle(BuildContext context) {
-  print(MediaQuery.of(context).size.height);
-  print(MediaQuery.of(context).size.height * 0.40);
-  return Container(
-    child:
-        Text("Hello! i am inside a container!", style: TextStyle(fontSize: 20)),
-    width: 0.3,
-    height: MediaQuery.of(context).size.height * 0.40,
-  );
-}
-*/
